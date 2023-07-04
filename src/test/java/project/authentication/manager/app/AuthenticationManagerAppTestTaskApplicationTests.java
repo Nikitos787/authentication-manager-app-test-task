9f31f1e558c093e6621a39cd2cef4b1e7dbac3ac
Nikita <nsalogub96@gmail.com>
2023-07-04 11:26:07 +0300

    preparation

---
+++ src/test/java/project/authentication/manager/app/AuthenticationManagerAppTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package project.authentication.manager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManagerAppTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/project/authentication/manager/app/TestAuthenticationManagerAppTestTaskApplication.java
@@ -0,0 +1,22 @@
+package project.authentication.manager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestAuthenticationManagerAppTestTaskApplication {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>("mysql:latest");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(AuthenticationManagerAppTestTaskApplication::main).with(TestAuthenticationManagerAppTestTaskApplication.class).run(args);
+    }
+
+}
